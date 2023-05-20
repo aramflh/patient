306dd3be8cd99d5489c02716fc14e94fa0d4eb32
Aramson <82621068+aramflh@users.noreply.github.com>
2023-05-20 12:45:17 +0200

    Import models

--- initializers/database.go
+++ initializers/database.go
@@ -48,6 +48,10 @@ func GenStructFromDB() {
 	g := gen.NewGenerator(gen.Config{
 		// Generate global variable Q as DAO interface, then you can query data like: dal.Q.User.First()
 		Mode: gen.WithDefaultQuery,
+		// Output destination folder for the generator, default value: ./query
+		OutPath: "dal",
+		// Query code file name, default value: gen.go
+		OutFile: "queryCode.go",
 		// if you want the nullable field generation property to be pointer type, set FieldNullable true
 		FieldNullable: true,
 		// if you want to assign field which has a default value in the `Create` API, set FieldCoverable true, reference: https://gorm.io/docs/create.html#Default-Values
--- main.go
+++ main.go
@@ -1,18 +1,19 @@
 package main
 
 import (
-	"dossier-patient/initializers"
 	"github.com/gin-gonic/gin"
 	"net/http"
-	// "database/sql"
-	// "gorm.io/driver/postgres"
-	// "gorm.io/gorm"
+	"patient/initializers"
 )
+
+// Q initializes a query var
+//var Q dal.Query
 
 // init loads before the main function
 func init() {
 	initializers.LoadEnvVariables()
 	initializers.ConnectToDB()
+	// Transform the tables in the DB into struct in dal/ directory
 	initializers.GenStructFromDB()
 }
 
@@ -21,7 +22,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
+			"message": "user",
 		})
 	})
 	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
